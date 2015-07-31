31cc353a842c6e0d1e762902047affed92345eb0
pasonx <619765945@qq.com>
2015-07-31 15:25:16 +0800

    Update main.c

--- main.c
+++ main.c
@@ -7,12 +7,39 @@ int main()
 {
     struct node_st * list = NULL;
     struct student_st node;
+    struct node_st * cur;
     for (int i = 0 ; i < 7 ; i ++) 
     {   
         node.id = i;
         snprintf(node.name,MAXSIZE,"studnet %d",i);
         node.math = rand() % 100;
 
-        list(&list,&node);
-    }    
+        insert_list(&list,&node);
+    }
+
+    //遍历
+    cur = list;
+	while (cur != NULL)
+	{
+		
+		printf("%d %s %d\n",cur->data.id,cur->data.name,cur->data.math);
+		cur = cur->next;	
+	}
+ 
+	
+	printf("\t\n\n");
+    //删除节点
+    delete_list(&list);
+    cur = list;
+	while (cur != NULL)
+	{
+		printf("%d %s %d\n",cur->data.id,cur->data.name,cur->data.math);
+		cur = cur->next;	
+	}
+	printf("\t\n\n");
+	  
+	cur = find_list(&list,3);
+	printf("%d %s %d\n",cur->data.id,cur->data.name,cur->data.math);
+	return 0;
+	
 }
