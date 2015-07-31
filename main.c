aa3ba91716ab722b469027b4a78b4e35ab1be0b2
pasonx <619765945@qq.com>
2015-07-31 13:26:29 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "nohead.h"
+
+int main()
+{
+    struct node_st * list = NULL;
+    struct student_st node;
+    for (int i = 0 ; i < 7 ; i ++) 
+    {   
+        node.id = i;
+        snprintf(node.name,MAXSIZE,"studnet %d",i);
+        node.math = rand() % 100;
+
+        list(&list,&node);
+    }    
+}
